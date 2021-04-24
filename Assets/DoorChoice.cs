9d7a28d7ba6f6ea259bdde20648255c24fec98ed
Thomas Ciampossin <thomasciampossin@live.fr>
2021-04-24 16:14:50 +0200

    PolyvalentRoom

---
+++ Assets/DoorChoice.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorChoice : MonoBehaviour
+{
+    public GameObject door;
+    public GameObject wall;
+
+    public void BeWall()
+    {
+
+    }
+    public void BeDoor()
+    {
+
+    }
+}
