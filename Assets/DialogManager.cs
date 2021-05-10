d776b3c32bfca65f51b4f398a0e6595b3c9d6fe6
Thomas Ciampossin <thomasciampossin@live.fr>
2021-05-10 18:08:34 +0200

    Dialog

---
+++ Assets/DialogManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogManager : MonoBehaviour
+{
+    public GameObject dialogBox;
+    public TextMeshProUGUI dialogText;
+    public string[] dialogLines;
+
+    public void LaunchDialog(DialogContainer target)
+    {
+
+    }
+}
--- Assets/PlayerRaycastingSystem.cs
+++ Assets/PlayerRaycastingSystem.cs
@@ -4,7 +4,34 @@ using UnityEngine;
 
 public class PlayerRaycastingSystem : MonoBehaviour
 {
+    public bool hovering;
+    public Color hoveringColor;
+    private Vector3 rayOrigin = new Vector3(0.5f, 0.5f, 0);
 
+    public float talkingDistance;
+    LayerMask dialogMask;
 
-     
+    private void Awake()
+    {
+        dialogMask = LayerMask.GetMask("Talkable");
+    }
+
+    private void FixedUpdate()
+    {
+        Ray ray = Camera.main.ViewportPointToRay(rayOrigin);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, talkingDistance, dialogMask))
+        {
+            hovering = true;
+            if (Input.GetButtonDown("Interact"))
+            {
+                //StartDialog
+            }
+        }
+        else
+        {
+            hovering = false;
+        }
+    }
 }
